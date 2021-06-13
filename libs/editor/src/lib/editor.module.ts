7b692026abfefaa0f0b935345608494e32d164b2
Joaquin Cid <joaqcid@gmail.com>
2021-06-13 11:39:14 -0300

    feat: editor library

---
+++ libs/editor/src/lib/editor.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  imports: [CommonModule, RouterModule.forChild([{ path: '', pathMatch: 'full', component: EditorPage }])]
+})
+export class EditorModule {}
